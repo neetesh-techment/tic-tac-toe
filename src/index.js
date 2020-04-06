4e46c717156f07f3f9789d42ae486bbb25d2a24c
Neetesh Gupta <neetesh.g@techment.com>
2020-04-06 12:03:20 +0530

    square component added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Square from './components/Square';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Square />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
