6fdd8933a59306a3d197b557525872de762da29d
Neetesh Gupta <neetesh.g@techment.com>
2020-04-07 16:06:51 +0530

    maintain state through parent component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
 
   render() {
     return (
